9b3d0cc17167c8483fe5f2ead1d817fae17d5f7c
bornon13oct <bornon13oct@gmail.com>
2017-04-20 19:34:35 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("*", function(req, res){
+    res.render("home");
+});
+
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("server has started");
+});
